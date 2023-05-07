ffcaf78e0a4d4e5836a73a82b8a4d9b369e25f3b
Johnny Quesada <johnny-qc@hotmail.com>
2023-05-07 19:55:06 +0200

    updating react-global-state-hooks
    updating git flow and reference of react global state hooks

--- src/dashboard/_shared/stores/asideState.ts
+++ src/dashboard/_shared/stores/asideState.ts
@@ -1,29 +1,30 @@
-import { GlobalStore, StoreTools } from "react-global-state-hooks";
+import { createGlobalState, StoreTools } from "react-global-state-hooks";
 
 export type AsideState = {
   isMenuOpen: boolean;
 };
 
-const asideStore = new GlobalStore(
+const initialState: AsideState = {
+  isMenuOpen: true,
+};
+
+export const useAsideState = createGlobalState(
   {
     isMenuOpen: true,
-  } as AsideState,
-  null,
+  },
   {
-    open() {
-      return ({ setState }: StoreTools<AsideState>) => {
-        setState((state) => ({ ...state, isMenuOpen: true }));
-      };
-    },
+    actions: {
+      open() {
+        return ({ setState }: StoreTools<typeof initialState>) => {
+          setState((state) => ({ ...state, isMenuOpen: true }));
+        };
+      },
 
-    close() {
-      return ({ setState }: StoreTools<AsideState>) => {
-        setState((state) => ({ ...state, isMenuOpen: false }));
-      };
-    },
-  } as const
+      close() {
+        return ({ setState }: StoreTools<typeof initialState>) => {
+          setState((state) => ({ ...state, isMenuOpen: false }));
+        };
+      },
+    } as const,
+  }
 );
-
-export const useAsideState = asideStore.getHook();
-
-export const [_, asideState] = asideStore.getHookDecoupled();
